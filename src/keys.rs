d7a1966db4748096e13eb474f075b1c3ebddf676
David Tran <tranhuyducseven@gmail.com>
2025-05-21 15:03:07 +0700

    feat/key: aggregate keys

--- src/keys.rs
+++ src/keys.rs
@@ -8,9 +8,16 @@
 
 use bitcoin::{
     hashes::{sha256::Hash as Sha256Hash, Hash},
-    secp256k1::Scalar,
+    hex::DisplayHex,
+    secp256k1::{self, SecretKey},
     PublicKey, TapTweakHash,
 };
+use k256::{
+    elliptic_curve::{bigint::U256, scalar::FromUintUnchecked},
+    Scalar,
+};
+
+use crate::tags::{KeyAggCoeffHash, KeyAggListHash};
 
 use super::{MuSig2, MuSig2Error};
 
@@ -90,26 +97,392 @@ impl KeyAggOption {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct AggregateKey {
     final_key: PublicKey,
     pre_tweaked_key: PublicKey,
 }
 
-impl<'a> MuSig2<'a> {
+impl MuSig2<'_> {
     pub fn aggregate_keys(
         &self,
         keys: &[PublicKey],
-        options: Vec<KeyAggOption>,
+        sort: bool,
+        modifiers: Option<&[KeyAggOptionModifer]>,
     ) -> Result<(AggregateKey, Scalar, Scalar), MuSig2Error> {
-        unimplemented!()
+        let mut opts = KeyAggOption::new();
+        if let Some(modifiers) = modifiers {
+            opts.apply(modifiers);
+        }
+
+        let mut sorted_keys = keys.to_owned();
+        if sort {
+            Self::sort_keys(&mut sorted_keys);
+        }
+
+        if opts.challenge_factor.is_none() {
+            opts.challenge_factor = Some(Self::caculate_challenge_factor(&sorted_keys));
+        }
+
+        if opts.unique_key_index.is_none() {
+            opts.unique_key_index = Some(Self::second_unique_key_index(&sorted_keys));
+        }
+
+        let mut combined_key = None;
+
+        for (i, key) in sorted_keys.iter().enumerate() {
+            let a = self.aggregation_coefficient(
+                &sorted_keys,
+                key,
+                opts.challenge_factor.as_ref().unwrap(),
+                opts.unique_key_index.unwrap(),
+            );
+
+            // Note: Need to deep dive into concerning about Jacobian coordinates
+            let tweaked_key = key.inner.mul_tweak(self.secp, &a)?;
+
+            if i == 0 {
+                combined_key = Some(tweaked_key);
+            } else {
+                combined_key = Some(combined_key.unwrap().combine(&tweaked_key)?);
+            }
+        }
+
+        let final_key =
+            combined_key.ok_or_else(|| MuSig2Error::Other(anyhow::anyhow!("No keys provided")))?;
+
+        // Store the pre-tweaked key for script path proofs
+        let combined_key = PublicKey::new(final_key);
+
+        // Handle taproot tweaking if enabled
+        if opts.taproot_tweak {
+            // Convert to x-only public key format
+            let (key_bytes, _) = combined_key.inner.x_only_public_key();
+            let key_slice = key_bytes.serialize();
+
+            // Use empty tweak bytes for BIP-0086, otherwise use provided tweak
+            let tweak_bytes: Vec<u8> = if opts.bip86_tweak {
+                vec![]
+            } else if !opts.tweaks.is_empty() {
+                opts.tweaks[0].tweak.to_byte_array().to_vec()
+            } else {
+                vec![]
+            };
+
+            // Compute taproot tagged hash: h_tapTweak(data)
+            // data = internalKey || tweakBytes
+
+            let mut data = Vec::new();
+            data.extend_from_slice(&key_slice);
+            data.extend_from_slice(&tweak_bytes);
+            let tap_tweak_hash = TapTweakHash::hash(&data);
+
+            // overwrite the tweak with the tap_tweak_hash
+            if !opts.tweaks.is_empty() {
+                opts.tweaks[0].tweak = tap_tweak_hash;
+            } else {
+                opts.tweaks.push(KeyTweakDesc {
+                    tweak: tap_tweak_hash,
+                    is_x_only: true,
+                });
+            }
+        }
+
+        let mut parity_acc = Scalar::ONE;
+        let mut tweak_acc = Scalar::ZERO;
+        let mut final_tweak_key_acc = final_key;
+
+        for tweak in opts.tweaks.iter() {
+            self.tweak_key(
+                &mut final_tweak_key_acc,
+                &mut parity_acc,
+                &mut tweak_acc,
+                &tweak.tweak,
+                tweak.is_x_only,
+            )?;
+        }
+
+        let final_tweaked_key = PublicKey::new(final_tweak_key_acc);
+
+        Ok((
+            AggregateKey {
+                pre_tweaked_key: combined_key,
+                final_key: final_tweaked_key,
+            },
+            parity_acc,
+            tweak_acc,
+        ))
+    }
+
+    /// c_i = H(c_all || P_i)
+    /// This function is used to calculate the aggregation coefficient for a given key
+    /// If the target key is the same as the key at the second key index, return 1
+    /// Otherwise, return the aggregation coefficient for the given key
+    /// For example:
+    /// Keys = [K1, K2, K3, K1, K2]
+    /// Second key index = 1
+    /// Keys[0] -> c_i = H(c_all || K1)
+    fn aggregation_coefficient(
+        &self,
+        key_set: &[PublicKey],
+        target_key: &PublicKey,
+        challenge_factor: &Sha256Hash,
+        second_key_idx: i32,
+    ) -> bitcoin::secp256k1::Scalar {
+        if second_key_idx != -1 && key_set[second_key_idx as usize] == *target_key {
+            return bitcoin::secp256k1::Scalar::ONE;
+        }
+
+        let mut coefficient_bytes = [0u8; 65];
+        coefficient_bytes[..32].copy_from_slice(challenge_factor.as_ref());
+        coefficient_bytes[32..].copy_from_slice(&target_key.inner.serialize());
+
+        let hash = KeyAggCoeffHash::hash(&coefficient_bytes);
+
+        bitcoin::secp256k1::Scalar::from_be_bytes(hash.into()).unwrap()
+    }
+
+    fn tweak_key(
+        &self,
+        key: &mut secp256k1::PublicKey,
+        parity_acc: &mut Scalar,
+        tweak_acc: &mut Scalar,
+        tweak: &TapTweakHash,
+        x_only: bool,
+    ) -> Result<(), MuSig2Error> {
+        // First compute the parity factor based on y-coordinate
+        let parity_factor = if x_only && !Self::has_even_y(key) {
+            Scalar::ONE.negate()
+        } else {
+            Scalar::ONE
+        };
+
+        let converted_parity_factor = {
+            let mut data: [u8; 32] = [0; 32];
+            data.copy_from_slice(&parity_factor.to_bytes());
+            secp256k1::Scalar::from_be_bytes(data)
+                .map_err(|_| MuSig2Error::Other(anyhow::anyhow!("Invalid parity factor")))?
+        };
+
+        // Compute g*Q (where g is parity_factor)
+        let mut tweaked_key = key.mul_tweak(self.secp, &converted_parity_factor)?;
+
+        let tweak_scalar = SecretKey::from_slice(tweak.as_byte_array())
+            .map_err(|_| MuSig2Error::Other(anyhow::anyhow!("Invalid tweak")))?;
+
+        // Compute t*G (where t is tweak_scalar)
+        let generator_point = secp256k1::PublicKey::from_secret_key(self.secp, &tweak_scalar);
+
+        // Add t*G to g*Q
+        tweaked_key = tweaked_key.combine(&generator_point)?;
+
+        // Update the accumulated values
+
+        let tweak_scalar = Scalar::from_uint_unchecked(U256::from_be_slice(tweak.as_byte_array()));
+
+        *parity_acc = parity_acc.mul(&parity_factor);
+        *tweak_acc = tweak_acc.mul(&parity_factor).add(&tweak_scalar);
+        *key = tweaked_key;
+
+        Ok(())
+    }
+
+    /// c_all = Tagged_Hash(P0, P1, ..., Pn)
+    pub fn caculate_challenge_factor(keys: &[PublicKey]) -> Sha256Hash {
+        KeyAggListHash::from_public_keys(keys).into()
+    }
+
+    /// This function is used to find the index of the second unique key in the list of keys
+    /// If all keys are the same, return -1
+    /// If there is only one key, return 0
+    fn second_unique_key_index(keys: &[PublicKey]) -> i32 {
+        for (i, key) in keys.iter().enumerate().skip(1) {
+            if key != &keys[0] {
+                return i as i32;
+            }
+        }
+        -1
+    }
+
+    fn sort_keys(keys: &mut [PublicKey]) {
+        if keys.is_sorted() {
+            return;
+        }
+        keys.sort();
+    }
+
+    fn has_even_y(key: &secp256k1::PublicKey) -> bool {
+        let key_bytes = key.serialize();
+        match key_bytes[0] {
+            0x02 => true,
+            0x03 => false,
+            _ => panic!("Invalid public key"),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
 
+    use std::str::FromStr;
+
+    use bitcoin::hex::DisplayHex;
+    use lazy_static::lazy_static;
+
     use super::*;
+
+    lazy_static! {
+        static ref MOCK_KEYS: Vec<PublicKey> = {
+            let mut keys = vec![
+                PublicKey::from_slice(
+                    &[0x02]
+                        .iter()
+                        .chain([0x08; 32].iter())
+                        .copied()
+                        .collect::<Vec<u8>>(),
+                )
+                .unwrap(),
+                PublicKey::from_slice(
+                    &[0x02]
+                        .iter()
+                        .chain([0x01; 32].iter())
+                        .copied()
+                        .collect::<Vec<u8>>(),
+                )
+                .unwrap(),
+                PublicKey::from_slice(
+                    &[0x02]
+                        .iter()
+                        .chain([0x07; 32].iter())
+                        .copied()
+                        .collect::<Vec<u8>>(),
+                )
+                .unwrap(),
+            ];
+
+            MuSig2::sort_keys(&mut keys);
+            keys
+        };
+    }
+
+    #[test]
+    fn test_simple_aggregate_keys() {
+        let keys = MOCK_KEYS.clone();
+        let secp = bitcoin::secp256k1::Secp256k1::new();
+        let musig2 = MuSig2::new(&secp);
+        let (agg, parity_acc, tweak_acc) = musig2.aggregate_keys(&keys, true, None).unwrap();
+
+        assert_eq!(
+            "02fa4785e10a19dae7c518c69dbac060a22be49a43e8c9b8a71d8f933a91640443",
+            agg.final_key.inner.serialize().to_lower_hex_string()
+        );
+
+        assert_eq!(
+            "0000000000000000000000000000000000000000000000000000000000000001",
+            parity_acc.to_bytes().to_lower_hex_string()
+        );
+
+        assert_eq!(
+            "0000000000000000000000000000000000000000000000000000000000000000",
+            tweak_acc.to_bytes().to_lower_hex_string()
+        )
+    }
+
+    #[test]
+    fn test_aggregate_keys_with_tweak() {
+        let keys = MOCK_KEYS.clone();
+        let secp = bitcoin::secp256k1::Secp256k1::new();
+        let musig2 = MuSig2::new(&secp);
+
+        let options: Vec<KeyAggOptionModifer> =
+            vec![Box::new(KeyAggOption::with_tweaks(vec![KeyTweakDesc {
+                tweak: TapTweakHash::from_byte_array([0x02; 32]),
+                is_x_only: true,
+            }]))];
+
+        let (agg, parity_acc, tweak_acc) =
+            musig2.aggregate_keys(&keys, true, Some(&options)).unwrap();
+
+        assert_eq!(
+            "028408bedd457c1f085788a8825c615b1f10544f53ece0d758b69fa4b294179b1a",
+            agg.final_key.inner.serialize().to_lower_hex_string()
+        );
+
+        assert_eq!(
+            "0000000000000000000000000000000000000000000000000000000000000001",
+            parity_acc.to_bytes().to_lower_hex_string()
+        );
+
+        assert_eq!(
+            "0202020202020202020202020202020202020202020202020202020202020202",
+            tweak_acc.to_bytes().to_lower_hex_string()
+        );
+    }
+
+    #[test]
+    fn test_tweak_key() {
+        //     pubkeyBytes, _ := hex.DecodeString("02fa4785e10a19dae7c518c69dbac060a22be49a43e8c9b8a71d8f933a91640443")
+
+        let mut pubkey = secp256k1::PublicKey::from_str(
+            "02fa4785e10a19dae7c518c69dbac060a22be49a43e8c9b8a71d8f933a91640443",
+        )
+        .unwrap();
+
+        let secp = secp256k1::Secp256k1::new();
+
+        let mut parity_acc = Scalar::ONE;
+        let mut tweak_acc = Scalar::ZERO;
+        let tweak = TapTweakHash::from_byte_array([0x1; 32]);
+
+        MuSig2::new(&secp)
+            .tweak_key(&mut pubkey, &mut parity_acc, &mut tweak_acc, &tweak, true)
+            .unwrap();
+
+        assert_eq!(
+            pubkey.serialize().to_lower_hex_string(),
+            "025f9d9693726d94483d7c83c17272158543c7366081511d231c3e6bfdbd87d94b"
+        );
+
+        assert_eq!(
+            parity_acc.to_bytes().to_lower_hex_string(),
+            "0000000000000000000000000000000000000000000000000000000000000001"
+        );
+
+        assert_eq!(
+            tweak_acc.to_bytes().to_lower_hex_string(),
+            "0101010101010101010101010101010101010101010101010101010101010101"
+        )
+    }
+
+    #[test]
+    fn test_arithmetic() {
+        let one_negated = Scalar::ONE.negate();
+        let mut data: [u8; 32] = [0x00; 32];
+        // copy one_negated to data
+        data.copy_from_slice(&one_negated.to_bytes().as_slice());
+
+        let one_negated = secp256k1::Scalar::from_be_bytes(data).unwrap();
+
+        assert_eq!(
+            one_negated.to_be_bytes().to_lower_hex_string(),
+            "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140"
+        )
+    }
+
+    #[test]
+    fn test_caculate_challenge_factor() {
+        let mut keys = MOCK_KEYS.clone();
+        MuSig2::sort_keys(&mut keys);
+        let challenge_factor = MuSig2::caculate_challenge_factor(&keys);
+        assert_eq!(
+            challenge_factor,
+            Sha256Hash::from_str(
+                "ba8ced89a6d45d58616bd38c4833e56da6aa6e2e384498b5a61a5c570ed23915"
+            )
+            .unwrap()
+        );
+    }
+
     #[test]
     fn test_key_options() {
         let mut opts = KeyAggOption::new();
--- src/tags.rs
+++ src/tags.rs
@@ -1,2 +1,85 @@
-pub const KEY_AGG_TAG_LIST: &[u8] = b"KeyAgg list";
-pub const KEY_AGG_TAG_COEFF: &[u8] = b"KeyAgg coefficient";
+use bitcoin::{
+    hashes::{
+        sha256::{self},
+        sha256t_hash_newtype, Hash, HashEngine,
+    },
+    PublicKey,
+};
+
+sha256t_hash_newtype! {
+    pub struct KeyAggCoeffTag = hash_str("KeyAgg coefficient");
+
+    /// Taproot-tagged hash with tag \"KeyAgg coefficient\".
+    ///
+    /// This is used for computing chanllenge factor for key aggregation.
+    // #[hash_newtype(forward)]
+    pub struct KeyAggCoeffHash(_);
+
+    pub struct KeyAggListTag = hash_str("KeyAgg list");
+
+    /// Taproot-tagged hash with tag \"KeyAgg list\".
+    ///
+    /// This is used for computing chanllenge factor for key aggregation.
+    #[hash_newtype(forward)]
+    pub struct KeyAggListHash(_);
+}
+
+impl KeyAggListHash {
+    /// Create a new [`KeyAggListHash`] from a list of public keys.
+    /// h = H(Tagged_Hash || Tagged_Hash || P0 || P1 || ... || Pn)
+    pub fn from_public_keys(keys: &[PublicKey]) -> KeyAggListHash {
+        let mut engine = KeyAggListHash::engine();
+        for key in keys {
+            engine.input(&key.inner.serialize());
+        }
+
+        KeyAggListHash::from_engine(engine)
+    }
+}
+
+impl From<KeyAggCoeffHash> for sha256::Hash {
+    fn from(val: KeyAggCoeffHash) -> Self {
+        sha256::Hash::from_byte_array(val.to_byte_array())
+    }
+}
+
+impl From<KeyAggCoeffHash> for [u8; 32] {
+    fn from(val: KeyAggCoeffHash) -> Self {
+        val.to_byte_array()
+    }
+}
+
+impl From<KeyAggListHash> for sha256::Hash {
+    fn from(val: KeyAggListHash) -> Self {
+        sha256::Hash::from_byte_array(val.to_byte_array())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_key_agg_list_hash() {
+        let keys = &vec![PublicKey::from_slice(
+            &[0x02]
+                .iter()
+                .chain([0x08; 32].iter())
+                .copied()
+                .collect::<Vec<u8>>(),
+        )
+        .unwrap()];
+
+        let h = KeyAggListHash::from_public_keys(keys);
+
+        assert_eq!(
+            h,
+            KeyAggListHash::from_str(
+                "9c63758509d81be9ed2be6089c71609c50f11f194241158e3863c1583e97a7b8"
+            )
+            .unwrap()
+        );
+    }
+}
